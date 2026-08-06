3555511fdaceaedda6116325233b1d3bcc8fbe0e
deep0436 <deepanshupatel0436@gmail.com>
2026-08-06 05:54:42 +0530

    Update main.tf

--- VPCwithALB/main.tf
+++ VPCwithALB/main.tf
@@ -1,122 +1,160 @@
 locals {
   description = "creating local variable"
-  name = "My"
+  name        = "My"
 }
 
-#Creating VPC
+#----------------------------------------------------
+# VPC
+#----------------------------------------------------
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
+
   tags = {
-      Name = "${local.name}-VPC"
+    Name = "${local.name}-VPC"
   }
 }
 
-#Create Internet Gateway IGW
+#----------------------------------------------------
+# Internet Gateway
+#----------------------------------------------------
 resource "aws_internet_gateway" "igw" {
- vpc_id = aws_vpc.myvpc.id
- tags = {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
     Name = "${local.name}-IGW"
- } 
+  }
 }
 
-#Attaching IGW to VPC
-resource "aws_internet_gateway_attachment" "igw" {
-  internet_gateway_id = aws_internet_gateway.igw.id
-  vpc_id = aws_vpc.myvpc.id
-}
-
-#Creating Public subnet in AZ ap-south-1a
+#----------------------------------------------------
+# Public Subnet
+#----------------------------------------------------
 resource "aws_subnet" "publicsubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.subnet1_cidr
-  availability_zone = var.az_1a
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.subnet1_cidr
+  availability_zone       = var.az_1a
   map_public_ip_on_launch = true
+
   tags = {
     Name = "${local.name}-Public-Subnet"
   }
 }
 
-#Creating Private Subnet in AZ ap-south-1b
+#----------------------------------------------------
+# Private Subnet
+# (Keeping as requested)
+#----------------------------------------------------
 resource "aws_subnet" "privatesubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.subnet2_cidr
-  availability_zone = var.az_1b
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.subnet2_cidr
+  availability_zone       = var.az_1b
   map_public_ip_on_launch = true
+
   tags = {
-    "Name" = "${local.name}-Private-Subnet"
+    Name = "${local.name}-Private-Subnet"
   }
 }
 
-#Creating NAT Gateway NAT-GW
-resource "aws_nat_gateway" "nat-gw" {
-  subnet_id = aws_subnet.privatesubnet.id
+#----------------------------------------------------
+# Elastic IP for NAT Gateway
+#----------------------------------------------------
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+}
+
+#----------------------------------------------------
+# NAT Gateway
+#----------------------------------------------------
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.publicsubnet.id
+
   tags = {
     Name = "${local.name}-NAT-GW"
   }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
 }
 
-#Creating Public Route Table
+#----------------------------------------------------
+# Public Route Table
+#----------------------------------------------------
 resource "aws_route_table" "publicRT" {
   vpc_id = aws_vpc.myvpc.id
+
   route {
     cidr_block = var.alltraffic
-    gateway_id = aws_internet_gateway.my-IGW.id
+    gateway_id = aws_internet_gateway.igw.id
   }
+
   tags = {
     Name = "${local.name}-PublicRT"
   }
 }
 
-#Creating Private Route Table
+#----------------------------------------------------
+# Private Route Table
+#----------------------------------------------------
 resource "aws_route_table" "privateRT" {
   vpc_id = aws_vpc.myvpc.id
+
   route {
     cidr_block     = var.alltraffic
-    nat_gateway_id = aws_nat_gateway.nat-gw.id
+    nat_gateway_id = aws_nat_gateway.natgw.id
   }
+
   tags = {
     Name = "${local.name}-PrivateRT"
   }
 }
 
-#Associate route table to Public RT
+#----------------------------------------------------
+# Route Table Association
+#----------------------------------------------------
 resource "aws_route_table_association" "publicRTA" {
+  subnet_id      = aws_subnet.publicsubnet.id
   route_table_id = aws_route_table.publicRT.id
-  subnet_id = aws_subnet.publicsubnet.id
 }
 
-#Associate route table to Private RT
 resource "aws_route_table_association" "privateRTA" {
-  route_table_id = aws_route_table.privateRT
-  subnet_id = aws_subnet.privatesubnet.id
+  subnet_id      = aws_subnet.privatesubnet.id
+  route_table_id = aws_route_table.privateRT.id
 }
 
-#Creating Security Groups
+#----------------------------------------------------
+# Security Group
+#----------------------------------------------------
 resource "aws_security_group" "websg" {
+  name   = "web-sg"
   vpc_id = aws_vpc.myvpc.id
 
   ingress {
-    description = "From web to VPC"
-    from_port = 80
-    to_port = 80
-    protocol = TCP
-    cidr_blocks = [ var.alltraffic ]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.alltraffic]
   }
 
   ingress {
     description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = TCP
-    cidr_blocks = [ var.alltraffic ]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.alltraffic]
   }
-  
+
   egress {
-    description = "Outbound open for all"
-    from_port = 0
-    to_port = 0
-    protocol = -1
-    cidr_blocks = [ var.alltraffic ]
+    description = "All Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.alltraffic]
   }
 
   tags = {
@@ -124,50 +162,72 @@ resource "aws_security_group" "websg" {
   }
 }
 
-#Creating S3 bucket
+#----------------------------------------------------
+# S3 Bucket
+#----------------------------------------------------
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bkt
 }
 
-#Creating EC2 instances in our created VPC
+#----------------------------------------------------
+# Jump Server
+#----------------------------------------------------
 resource "aws_instance" "jumpserver" {
-  ami = var.i_ami
-  subnet_id = aws_subnet.publicsubnet.id
-  instance_type = var.i_type  
-  security_groups = [ aws_security_group.websg.id ]
-  user_data = base64encode(file(jump_userdata.sh))
+  ami                    = var.i_ami
+  instance_type          = var.i_type
+  subnet_id              = aws_subnet.publicsubnet.id
+  vpc_security_group_ids = [aws_security_group.websg.id]
+
+  user_data = file("jump_userdata.sh")
+
   tags = {
     Name = "${local.name}-Web-Server"
   }
 }
 
-#Creating EC2 instances in our created VPC
+#----------------------------------------------------
+# DB Server
+#----------------------------------------------------
 resource "aws_instance" "dbserver" {
-  ami = var.i_ami
-  subnet_id = aws_subnet.privatesubnet.id
-  instance_type = var.i_type 
-  security_groups = [ aws_security_group.websg.id ]
-  user_data = base64encode(file(db_userdata.sh))
+  ami                    = var.i_ami
+  instance_type          = var.i_type
+  subnet_id              = aws_subnet.privatesubnet.id
+  vpc_security_group_ids = [aws_security_group.websg.id]
+
+  user_data = file("db_userdata.sh")
+
   tags = {
     Name = "${local.name}-DB-Server"
   }
 }
 
-#Creating Application Load Balancer
+#----------------------------------------------------
+# Application Load Balancer
+#----------------------------------------------------
 resource "aws_lb" "myalb" {
-  internal = false
+  name               = "${local.name}-ALB"
+  internal           = false
   load_balancer_type = "application"
-  name = "${local.name}-ALB"
-  security_groups = [ aws_security_group.websg.id ]
-  subnets = [ aws_subnet.publicsubnet.id,aws_subnet.privatesubnet.id ]
+
+  security_groups = [
+    aws_security_group.websg.id
+  ]
+
+  subnets = [
+    aws_subnet.publicsubnet.id,
+    aws_subnet.privatesubnet.id
+  ]
+
   tags = {
     Name = "${local.name}-ALB"
   }
 }
 
-#Creating Target Groups
+#----------------------------------------------------
+# Target Group
+#----------------------------------------------------
 resource "aws_lb_target_group" "tg" {
-  name = "${local.name}-App-TG"
+  name     = "${local.name}-App-TG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.myvpc.id
@@ -178,28 +238,31 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-#Attaching Load Balancer to Target group along with instance
+#----------------------------------------------------
+# Target Group Attachment
+#----------------------------------------------------
 resource "aws_lb_target_group_attachment" "tga1" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id = aws_instance.jumpserver.id
-  port = 80
+  target_id        = aws_instance.jumpserver.id
+  port             = 80
 }
 
-#Attaching Load Balancer to Target group along with instance
 resource "aws_lb_target_group_attachment" "tga2" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id = aws_instance.dbserver.id
-  port = 80
+  target_id        = aws_instance.dbserver.id
+  port             = 80
 }
 
-#Adding listener to ALB
+#----------------------------------------------------
+# Listener
+#----------------------------------------------------
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.myalb
-  port = 80
-  protocol = "HTTP"
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
+    type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
-    type = "forward"
   }
 }
