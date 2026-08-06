4d0c7c7551c1e75726d422f5934ae30485f1a421
deep0436 <deepanshupatel0436@gmail.com>
2026-08-06 05:47:03 +0530

    Add files via upload

--- VPCwithALB/backend.tf
+++ VPCwithALB/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "terraform-vpc-with-alb-project" #var.bkt
     key = "prod/terraform.tfstate"
     region = "ap-south-1"
-    use_use_lockfile = true    
+    use_lockfile = true    
   }
 }
