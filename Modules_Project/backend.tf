d9a79064a01dd084346dc7001308770339d86b26
deep0436 <deepanshupatel0436@gmail.com>
2026-08-10 16:34:21 +0530

    Update backend.tf

--- Modules_Project/backend.tf
+++ Modules_Project/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    Name = "terraf-backend-s3-demo"
+    bucket = "terraf-backend-s3-demo"
     key = "prod/terraform.tfstate"
+    region = "ap-south-1"
     use_lockfile = true
   }
 }
